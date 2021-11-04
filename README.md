072443c1722732c906d09c747504a77688b76603
hidayathamir <hidayathamir@gmail.com>
2021-11-04 23:29:22 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# telegram-forward-message-keyword
+
+Simple script to forward telegram new message that contain your keyword.
+
+## How To Run
+1. Get your api_id and api_hash in my.telegram.org
+2. Use your api_id and api_hash in get-dialogs.py and `main.py`.
+3. Run get-dialogs.py, 
+    ```
+    python3 get-dialogs.py
+    ```
+    input your telegram phone number and your login code, new file `dialogs.json` will be created contain chat_id and chat_name.
+4. Use chat_id from `dialogs.json` in senders and receiver in `main.py`
+5. Run `main.py`
+    ```
+    python3 main.py
+    ```
+6. Now every new message from senders that contain keyword will be forwarded to receiver.
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import re
 from typing import Set
 from decouple import config
-from telethon import TelegramClient, events
+from telethon import TelegramClient
+from telethon.events import NewMessage
 
 api_id = config("api_id", cast=int)
 api_hash = config("api_hash")
@@ -24,22 +25,23 @@ keywords = [
 
 
 def set_has_keyword(set_msg: Set[str]) -> bool:
-    for keyword in keywords:
-        if keyword in set_msg:
-            return True
-    return False
+    return any(keyword in set_msg for keyword in keywords)
 
 
-@client.on(events.NewMessage(incoming=True, chats=senders))
-async def forward(event):
+def get_only_alpanumber(msg_text: str) -> str:
+    return re.sub("[^0-9a-zA-Z ]", "", msg_text)
+
+
+@client.on(NewMessage(incoming=True, chats=senders))
+async def forward(event: NewMessage.Event) -> None:
     msg_text = event.message.text
-    msg_text = re.sub("[^0-9a-zA-Z ]", "", msg_text)
+    msg_text = get_only_alpanumber(msg_text)
     set_msg_text = set(msg_text.split())
     if set_has_keyword(set_msg_text):
         await client.send_message(receiver, event.message)
 
 
-def main():
+def main() -> None:
     print("running")
     client.start()
     client.run_until_disconnected()
